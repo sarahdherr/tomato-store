bc99a30f33d82cecc38ea2e8f219388bfd14a2f4
Sarah Herr <sarah.herr@hotmail.com>
2017-04-17 15:33:49 -0400

    get all products route started

---
+++ server/products.js
@@ -0,0 +1,13 @@
+// Rooted off `/api/products`
+
+'use strict'
+
+const {Product} = require('APP/db')
+const Product = db.model('products')
+
+module.exports = require('express').Router()
+  .get('/', function(req, res, next) {
+  	Product.findAll()
+  	  .then(products => res.send(products))
+  	  .catch(next)
+  })
