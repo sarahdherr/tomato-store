360e0db00c1280927a356d505df58bbe918be72e
Allison Alexander <aveleber@hotmailcom>
2017-04-17 16:22:10 -0400

    tweaks to pull request

--- db/models/product.test.js
+++ db/models/product.test.js
@@ -14,15 +14,7 @@ describe('Product', () => {
     it('has a specified name', () =>
       Product.create({ name: 'FDA Tomato 500' })
         .then(product => 
-          { console.log('PRODUCT', product.name)
-            expect.product.name.to.equal('FDA Tomato 500')
-        })
+            expect(product.name).to.equal('FDA Tomato 500'))
       )
-
     })
-    // it("resolves false if the name doesn't match", () =>
-    //   Product.create({ name: 'ok' })
-    //     .then(product => product.authenticate('not ok'))
-    //     .then(result => expect(result).to.be.false))
-   //})
 })
