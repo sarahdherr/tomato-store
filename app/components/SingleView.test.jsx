9345c41b18e2c20542f6ac0657ab918e25ffcd69
Stefanie Sulzer <stefanie.sulzer@gmail.com>
2017-04-18 12:27:55 -0400

    accidentally did work in master branch: plan ot merge into branch and then move the master branch back to before the commit

---
+++ app/components/SingleView.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import chai, {expect} from 'chai'
+chai.use(require('chai-enzyme')())
+import {shallow} from 'enzyme'
+import SingleView from './SingleView'
+
+describe('<SingleView />', () => {
+  const tomato = {
+    name: 'TomTom Tomato',
+    imageURL: 'https://www.ethno-botanik.org/Tomaten/Green_Sausage/Fotos/300/Green_Sausage_15.jpg',
+    price: 3,
+    quantity: 100,
+  }
+
+  let root
+  beforeEach('render the single view', () =>
+    root = shallow(<SingleView {...tomato} />)
+  )
+
+  it('renders with a name and image', () => {
+    expect(root.find('img')).to.have.length(1)
+    expect(root.find('h1').text()).equal(tomato.name)
+  })
+})
