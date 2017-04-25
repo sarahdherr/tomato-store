6cb98462f9cfa6e5d1853b819363a995e71c4100
Stefanie Sulzer <stefanie.sulzer@gmail.com>
2017-04-25 11:19:37 -0400

    added redirect after SignUp completed

--- app/components/SignUp.jsx
+++ app/components/SignUp.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router'
 
 export default class extends React.Component {
   constructor(props) {
@@ -24,6 +25,7 @@ export default class extends React.Component {
     evt.preventDefault()
     console.log(this.state)
     this.props.setUser(this.state)
+
   }
 
   render() {
@@ -46,7 +48,7 @@ export default class extends React.Component {
           <input name='password' onChange={this.handleChange} />
         </div>
 
-        <button type='submit' value='Sign Up'>SIGN UP</button>
+        <button type='submit' value='Sign Up'><Link to='/products'>SIGN UP</Link></button>
       </form>
     </div>
     )
