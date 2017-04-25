c1268d09f3bdcf94feb3aca55d4b41e689695160
Sarah Herr <sarah.herr@hotmail.com>
2017-04-25 11:03:07 -0400

    working on CORS problem in OAuth

--- server/auth.js
+++ server/auth.js
@@ -121,7 +121,7 @@ passport.use(new (require('passport-local').Strategy)(
   }
 ))
 
-app.use(function(req, res, next) {
+auth.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
