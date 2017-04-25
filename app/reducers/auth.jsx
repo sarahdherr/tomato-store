c3093e2bab834924be3fbc07df9cdc8531099a05
Sarah Herr <sarah.herr@hotmail.com>
2017-04-25 12:17:09 -0400

    fix(login/signup): clean up for pull request

--- app/reducers/auth.jsx
+++ app/reducers/auth.jsx
@@ -36,7 +36,6 @@ export const whoami = () =>
     axios.get('/api/auth/whoami')
       .then(response => {
         const user = response.data
-        console.log("USER: ", user)
         dispatch(authenticated(user))
       })
       .catch(failed => dispatch(authenticated(null)))
